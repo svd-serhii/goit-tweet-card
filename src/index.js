8829a438c4a752b208a5eed547cdc0235fb3e6db
Serhii Demchenko <svdemchenko@gmail.com>
2023-06-05 20:35:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-tweet-card">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
